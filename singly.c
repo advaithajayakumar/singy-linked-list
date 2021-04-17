fa4412b9498cfcf7766b95a14bec17a426d6f73d
advaitha jayakumar <advaithajayakumar@gmail.com>
2021-04-17 21:42:09 +0530

    opertations to insert element at the end

--- singly.c
+++ singly.c
@@ -32,9 +32,22 @@ int main(){
         head=newnode;
         
     }
+    void insert_at_end(int n){
+        newnode=(struct node*)malloc(sizeof(struct node));
+        newnode->data=n;
+        newnode->ptr=0;
+        temp=head;
+        while(temp->ptr!=0){
+            temp=temp->ptr;
+        }
+        temp->ptr=newnode;
+    }
     printf("enter the element to be inserted at the begining");
     scanf("%d",&m);
     insert_at_beg(m);
+    printf("enter the element you wish to insert at the end");
+    scanf("%d",&n);
+    insert_at_end(n);
     printf("displaying the elements");
     temp=head;
     while(temp!=0){
